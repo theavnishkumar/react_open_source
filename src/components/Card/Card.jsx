d0bdb5aef277abf46264b979c2b3f13476fdcac6
Aditya Kumar <avnishbxr@gmail.com>
2023-10-28 13:09:19 +0530

    Added Navbar & Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+
+function Card(props) {
+    
+  return (
+    <>
+    <div className="container m-5">
+    <div className="card" style={{width:'18rem'}}>
+  <img src="https://picsum.photos/200" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{props.title}</h5>
+    <p className="card-text">{props.description}</p>
+    <a href="#" className="btn btn-primary">Go</a>
+  </div>
+</div>
+    </div>
+    </>
+  
+  )
+}
+
+Card.propTypes={
+    title:PropTypes.string,
+    description:PropTypes.string
+}
+
+Card.defaultProps={
+    title:"Card Title",
+    description:"Card Description"
+}
+
+export default Card
