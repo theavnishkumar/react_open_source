72aff6499cd8784534bf87d4e4f72fc1422694ad
Aditya Kumar <avnishbxr@gmail.com>
2023-10-28 13:17:47 +0530

    .

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
   return (
     <>
     <div className="container m-5">
-    <div className="card" style={{width:'18rem'}}>
+    <div className="card" style={{width:'16rem'}}>
   <img src="https://picsum.photos/200" className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{props.title}</h5>
